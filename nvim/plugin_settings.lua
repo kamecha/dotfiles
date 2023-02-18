9861f4ef7f9066161832d86350e77754a509e4ae
kamecha <masakikameyama81@gmail.com>
2023-02-19 01:05:51 +0900

    LSPのRename機能へのマッピング

--- nvim/plugin_settings.lua
+++ nvim/plugin_settings.lua
@@ -13,6 +13,7 @@ local on_attach = function (client, bufnr)
 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', 'gn', vim.lsp.buf.rename, bufopts)
 	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 

72b1d3c360b290c2ba26485359eb0a45b28e00e8
kamecha <masakikameyama81@gmail.com>
2023-05-17 00:27:57 +0900

    tmuxのプラグイン本体は別ディレクトリに保存

--- setup.sh
+++ setup.sh
@@ -3,16 +3,52 @@ set -e
 
 cd ~
 
+# tmux関連の設定を行う関数を定義
+tmux_setup() {
+	# tmuxのインストール
+	if [ ! -f /usr/local/bin/tmux ]
+	then
+		# echo "Installing tmux..."
+		# sudo apt install -y tmux
+		echo "tmux is not installed."
+		return
+	else
+		echo "tmux already installed."
+	fi
+	# tmuxのプラグインマネージャ、tpmをインストール
+	if [ ! -d ~/.tmux/plugins/tpm ]
+	then
+		echo "Installing TPM..."
+		git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+	else
+		echo "TPM already installed."
+	fi
+	# .tmux.confをシンボリックリンクで貼る
+	ln -sfv "$(cd ~/dotfiles; pwd)/.tmux.conf" "$HOME/.tmux.conf"
+	# check tmux is already installed
+	if [ ! -f /usr/local/bin/tmux ]
+	then
+		echo "tmux is not installed."
+	else
+		echo "tmux already installed."
+		echo "source tmux.conf"
+		tmux source ~/.tmux.conf
+		echo "user prefix + I to install plugins"
+	fi
+}
+
 # OSごとに異なる設定
 case $(uname) in
 	"Linux" ) echo "Linux"
 		if [ ! -f /usr/local/bin/starship ]
-			then
-				echo "Installing Starship..."
-				curl -sS https://starship.rs/install.sh | sh
-			else
-				echo "Starship already installed."
+		then
+			echo "Installing Starship..."
+			curl -sS https://starship.rs/install.sh | sh
+		else
+			echo "Starship already installed."
 		fi
+		# tmuxのプラグインマネージャ、tpmをインストール
+		tmux_setup
 		break;;
 	"Darwin" ) echo "Mac"
 		echo "architectures: $(uname -m) によってbrewのインストール方法が異なるので保留"
@@ -37,15 +73,3 @@ ln -sfv "$(pwd)/.vim" "$HOME/"
 # for each shell
 ln -sfv "$(pwd)/.bashrc" "$HOME/.bashrc"
 ln -sfv "$(pwd)/.zshrc" "$HOME/.zshrc"
-# for tmux
-ln -sfv "$(pwd)/.tmux.conf" "$HOME/.tmux.conf"
-ln -sfv "$(pwd)/.tmux" "$HOME/"
-# check tmux is already installed
-if [ ! -f /usr/local/bin/tmux ]
-	then
-		echo "tmux is not installed."
-	else
-		echo "tmux already installed."
-		echo "source tmux.conf"
-		tmux source ~/.tmux.conf
-fi

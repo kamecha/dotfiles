bb6d68a6ef738e54be2efa76bd946a9614dc2bd1
kamecha <masakikameyama81@gmail.com>
2023-10-13 18:49:36 +0900

    dduのレイアウト周りをlua化

--- nvim/ddu.lua
+++ nvim/ddu.lua
@@ -118,3 +118,218 @@ vim.fn["ddu#custom#patch_global"]({
 		}
 	}
 })
+
+-- layout
+WinCol = 1
+WinRow = 1
+WinWidth = vim.api.nvim_get_option("columns")
+WinHeight = vim.api.nvim_get_option("lines")
+Columns = vim.api.nvim_get_option("columns")
+Lines = vim.api.nvim_get_option("lines")
+
+local function set_size()
+	-- 関数外部のwinColを参照して、columns / 8を代入する
+	WinCol = math.floor(Columns / 8)
+	WinRow = math.floor(Lines / 8)
+	WinWidth = math.floor(Columns - (Columns / 4))
+	WinHeight = math.floor(Lines - (Lines / 4))
+end
+
+local function set_ff_layout()
+	-- ┌────────────────────────────────┐
+	-- │                                │
+	-- │                                │
+	-- │        source                  │
+	-- │                                │
+	-- │                                │
+	-- │                                │
+	-- └────────────────────────────────┘
+	vim.fn["ddu#custom#patch_local"]('default', {
+		uiParams = {
+			ff = {
+				winCol = WinCol,
+				winRow = WinRow,
+				winWidth = WinWidth,
+				winHeight = WinHeight
+			}
+		}
+	})
+
+	-- ┌────────────────┐┌────────────────┐
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- │  source        ││                │
+	-- │                ││                │
+	-- │                ││   preview      │
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- └────────────────┘│                │
+	-- ┌────────────────┐│                │
+	-- │   filter       ││                │
+	-- └────────────────┘└────────────────┘
+	vim.fn["ddu#custom#patch_local"]('ui_filter_preview_layout', {
+		uiParams = {
+			ff = {
+				startFilter = true,
+				filterFloatingPosition = 'bottom',
+				autoAction = { name = 'preview' },
+				startAutoAction = true,
+				winCol = WinCol,
+				winRow = WinRow,
+				winWidth = math.floor(WinWidth / 2),
+				winHeight = WinHeight,
+				previewCol = WinCol + math.floor(WinWidth / 2) + 2,
+				previewRow = WinRow + WinHeight + 5,
+				previewWidth = math.floor(WinWidth / 2),
+				previewHeight = WinHeight + 3,
+			}
+		}
+	})
+
+	-- ┌────────────────┐
+	-- │                │
+	-- │   preview      │
+	-- │                │
+	-- └────────────────┘
+	-- ┌────────────────┐
+	-- │   source       │
+	-- │                │
+	-- └────────────────┘
+	-- ┌────────────────┐
+	-- │   filter       │
+	-- └────────────────┘
+	vim.fn["ddu#custom#patch_local"]('ui_filter_preview_horizontal_layout', {
+		uiParams = {
+			ff = {
+				startFilter = true,
+				filterFloatingPosition = 'bottom',
+				autoAction = { name = 'preview' },
+				startAutoAction = true,
+				previewSplit = 'horizontal',
+				winCol = WinCol,
+				winRow = math.floor(Lines / 2),
+				winWidth = WinWidth,
+				winHeight = math.floor(WinHeight / 2),
+				previewCol = WinCol,
+				previewRow = 0,
+				previewWidth = WinWidth,
+				previewHeight = math.floor(WinHeight / 2),
+			}
+		}
+	})
+
+	-- ┌────────────────┐
+	-- │                │
+	-- │   source       │
+	-- │                │
+	-- └────────────────┘
+	-- ┌────────────────┐
+	-- │   filter       │
+	-- └────────────────┘
+	vim.fn["ddu#custom#patch_local"]('ui_filter_layout', {
+		uiParams = {
+			ff = {
+				startFilter = true,
+				filterFloatingPosition = 'bottom',
+				winCol = WinCol,
+				winWidth = WinWidth,
+				winRow = WinRow,
+				winHeight = WinHeight,
+			}
+		}
+	})
+
+	-- ┌────────────────┐┌────────────────┐
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- │  source        ││   preview      │
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- └────────────────┘└────────────────┘
+	vim.fn["ddu#custom#patch_local"]('ui_preview_layout', {
+		uiParams = {
+			ff = {
+				autoAction = { name = 'preview' },
+				startAutoAction = true,
+				previewSplit = 'vertical',
+				winCol = WinCol,
+				winWidth = math.floor(WinWidth / 2),
+				winRow = WinRow,
+				winHeight = WinHeight,
+				previewCol = WinCol + math.floor(WinWidth / 2) + 2,
+				previewWidth = math.floor(WinWidth / 2),
+				previewRow = WinRow,
+				previewHeight = WinHeight,
+			}
+		}
+	})
+end
+
+local function set_filer_layout()
+	-- ┌────────────────────────────────┐
+	-- │                                │
+	-- │                                │
+	-- │        source                  │
+	-- │                                │
+	-- │                                │
+	-- │                                │
+	-- └────────────────────────────────┘
+	vim.fn["ddu#custom#patch_local"]('default', {
+		uiParams = {
+			filer = {
+				winCol = WinCol,
+				winRow = WinRow,
+				winWidth = WinWidth,
+				winHeight = WinHeight,
+			}
+		}
+	})
+
+	-- ┌────────────────┐┌────────────────┐
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- │  source        ││   preview      │
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- │                ││                │
+	-- └────────────────┘└────────────────┘
+	vim.fn["ddu#custom#patch_local"]('ui_filer_preview_layout', {
+		uiParams = {
+			filer = {
+				autoAction = { name = 'preview' },
+				startAutoAction = true,
+				previewSplit = 'vertical',
+				winCol = WinCol,
+				winWidth = math.floor(WinWidth / 2),
+				winRow = WinRow,
+				winHeight = WinHeight,
+				previewCol = WinCol + math.floor(WinWidth / 2) + 2,
+				previewWidth = math.floor(WinWidth / 2),
+				previewRow = WinRow,
+				previewHeight = WinHeight,
+			}
+		}
+	})
+end
+
+local function layout()
+	set_size()
+	set_ff_layout()
+	set_filer_layout()
+end
+
+layout()
+
+vim.api.nvim_create_autocmd("VimResized", {
+	pattern = "*",
+	callback = function()
+		layout()
+	end
+})

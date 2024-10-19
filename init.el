5aab7ae4bf5651cbf8897e0fd0c95e8ff65afe94
kamecha <masakikameyama81@gmail.com>
2024-10-19 17:05:29 +0900

    Emacsにパッケージマネージャとしてuse-packageを使用

--- init.el
+++ init.el
@@ -1,3 +1,19 @@
+(require 'package)
+
+;; package-archivesを上書き
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+	("org" . "https://orgmode.org/elpa/")
+	("gnu" . "https://elpa.gnu.org/packages/")))
+
+;; 所期化
+(package-initialize)
+
+;; use-packageがなかったら入れるようにする
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 ; delete by <C-h>
 (keyboard-translate ?\C-h ?\C-?)
 
